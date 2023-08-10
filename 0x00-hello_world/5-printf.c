eb7828c502610591072148b647cf6a72fd2b7fc2
Marwa Sedky <marwa.sedky2011@gmail.com>
2023-08-10 15:02:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: app that print message
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+
+}
