095752a9e8ab6fa252de5635fff09e25e5c5b1dd
Marwa Sedky <marwa.sedky2011@gmail.com>
2023-08-10 14:51:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: app that print message
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
