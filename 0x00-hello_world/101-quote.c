0beab3615cc72c68e3ffa84a58726cb6414bc82c
Marwa Sedky <marwa.sedky2011@gmail.com>
2023-08-10 16:35:06 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: app that print message
+ *
+ * Return: Always 1
+*/
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, str, 59);
+	return (1);
+}
