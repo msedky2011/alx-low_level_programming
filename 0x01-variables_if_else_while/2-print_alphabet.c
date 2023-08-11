e02a48909cdfff3689cd20d89a176e7d47cdfe3b
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-12 02:48:15 +0300

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Success
+*/
+
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar (alph);
+	}
+	putchar ('\n');
+	return (0);
+}
