cc5ed396483144518ec4d0ff73cac6fd9373e9d9
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-12 03:31:44 +0300

    5-print_numbers_ed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Success
+*/
+
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	putchar ('\n');
+	return (0);
+}
