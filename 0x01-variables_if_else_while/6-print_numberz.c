f9d353d1c79ea0ab7197c1b42305ec98892ce16b
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-12 03:41:11 +0300

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Success
+*/
+
+
+int main(void)
+{
+	char alph;
+
+	for (alph = '0'; alph <= '9'; alph++)
+	{
+		putchar (alph);
+	}
+	putchar ('\n');
+	return (0);
+}
