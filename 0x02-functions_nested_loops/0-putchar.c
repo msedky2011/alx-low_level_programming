47a398a335f04401055b221e6bbea584c8d2d227
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-15 20:40:58 +0300

    0-putchar.c_edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,12 @@
  * Return: 0 Success
 */
 
-
 int main(void)
 {
 	int e;
 	char str[] = "_putchar";
-	
-	for (e = 0; e <= 8; e++)
-		_putchar (str[e]);
+		for (e = 0; e <= 8; e++)
+			_putchar (str[e]);
 	_putchar ('\n');
 	return (0);
 }
