2eef8b4587f0eb16b7c217c38a74a89df1108657
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-16 00:06:24 +0300

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_sign - check number positive or negative
+ *@n:character want to check
+ *Return: 1 if positive
+ *return:0 if zero
+ *	: -1 if negative
+ **/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar (43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
