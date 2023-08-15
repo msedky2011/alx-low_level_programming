91a6a19ce00d0aa51c992fd93ac557c8c867dacd
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-15 22:30:30 +0300

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,9 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 		for (j = 0 ; j <= 9 ; j++)
+		{
 		for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+			_putchar(i);
+		_putchar('\n');
+		}
 }
