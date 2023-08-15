b1f48507b138280017fa3c772c8712c2471072b7
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-15 22:26:40 +0300

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - put alphabet in lower case 10 times
+ * Return: 0 Success
+ **/
+void print_alphabet_x10(void)
+{
+	int i, j;
+		for (j = 0 ; j <= 9 ; j++)
+		for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
