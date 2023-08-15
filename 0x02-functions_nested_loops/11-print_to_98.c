3a394a3e49490b87311ffd743a16f503f47e0192
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-16 02:23:02 +0300

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - entry point
+ * @n: first number
+ **/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("%d\n", 98);
+}
