b2c308e9d81321b373326a5b3bc3b0d95e37d76c
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-16 10:25:32 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,21 +12,21 @@ int main(void)
 	long x;
 	long y;
 
-	x=1;
-	y=1;
+	x = 1;
+	y = 1;
 
 	sum = 0;
-	printf("%d",2);
-	y=2;
+	printf("%d", 1);
+	y = 1;
 
 	for (i = 1; i < 50; i++)
 	{
 		sum = x + y;
 		x = y;
 		y = sum;
-		printf(", %ld", sum); 
+		printf(", %ld", sum);
 	}
-	
+
 	printf("\n");
 
 	return (0);
