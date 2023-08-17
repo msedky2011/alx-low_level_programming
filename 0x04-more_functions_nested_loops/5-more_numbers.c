57aa33cdc42db6b739c2d490f6eff85cb6d56d86
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-17 21:37:25 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*more_numbers- _print digit from 0 to 14 ten times
+*_putchar-used to print on stdout
+*/
+
+void more_numbers(void)
+{
+	int i, j, z;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			z = j;
+			if (z > 9)
+			{
+				_putchar(49);
+				z = j % 10;
+			}
+			_putchar(z + 48);
+		}
+		_putchar('\n');
+	}
+}
