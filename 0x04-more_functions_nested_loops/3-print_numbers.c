0900a430e37b579095f0fea7187eda47dcb7ed78
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-17 20:24:08 +0300

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers- _print digit from 0 to 9
+*_putchar-used to print on stdout
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
