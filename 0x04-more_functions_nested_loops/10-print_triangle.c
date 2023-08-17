fb722c74c2e5df7207256a63e6d3adad403a66b8
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-18 00:08:54 +0300

    10-triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_diagonal - print times n to stdout
+*@n: number of print
+*/
+
+void print_triangle(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n -1 -i; j++)
+				_putchar(' ');
+			for (j = n-1-i; j < n ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
