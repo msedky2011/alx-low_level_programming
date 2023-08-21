4c3fd721a869a9a21726b310f004ce446189617e
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-22 01:55:32 +0300

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ *print_rev- prints a string, in reverse
+ *
+ * @s:char parameter
+ *
+ * Return:nothing
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i])
+	i++;
+	while (i--)
+		_putchar(s[i] + 0);
+	_putchar('\n');
+}
