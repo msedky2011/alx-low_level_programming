10e675fe26e3f0a3dca494279f4c9e925388db69
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-22 02:51:10 +0300

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,30 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ *rev_string- prints a string, in reverse
+ *
+ * @s:char parameter
+ *
+ * Return:nothing
+ *
+ */
+void rev_string(char *s)
+{
+	int i,j;
+	char k;
+
+	i = 0;
+
+	if (s[i] != '\0')
+	{
+	while (s[i + 1] != '\0')
+		i++;
+	for (j = 0; j <= i / 2; j++)
+	{
+		k = s[i - j];
+		s[i - j] = s[j];
+		s[j] = k;
+	}
+	}
+}
