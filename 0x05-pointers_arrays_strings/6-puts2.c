6b3b54a2db8a959674fee026d32a502f04333001
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-22 03:12:01 +0300

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ *puts2- function that returns the length of a string
+ *
+ * @str:char parameter
+ *
+ * Return:nothing
+ *
+ */
+void puts2(char *str)
+{
+	for (; *str != '\0' ; str += 2)
+	{
+		_putchar(*str + 0);
+		if (*str + 1 == '\0')
+			break;
+	}
+	_putchar('\n');
+}
