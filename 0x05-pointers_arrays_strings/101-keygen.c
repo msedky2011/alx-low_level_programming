bed299720b5aa85a82ad5f63f7ef99ec3d0e885e
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-22 16:12:05 +0300

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	srand(time(NULL));
+	int i;
+	char pass[9];
+	
+	for (i = 0; i < 8 ; i++)
+		pass[i] = ('!' + (rand() % 95));
+	printf ("%s \n", pass);
+
+
+    return (0);
+}
