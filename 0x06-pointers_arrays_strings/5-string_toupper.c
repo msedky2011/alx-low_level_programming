13c89feeac409b1e6e4dbd0518e4f342d7d75ed1
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-24 01:59:20 +0300

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ *string_toupper- that concatenates two strings.
+ *
+ *@a: array
+ *
+ *
+ * Return: char
+ */
+
+
+char *string_toupper(char *a)
+{
+	int j;
+
+for (j = 0 ; a[j] != '\0'; j++)
+{
+	if (a[j] >= 97 && a[j] <= 122)
+		a[j] = a[j] - 32;
+}
+return (a);
+}
