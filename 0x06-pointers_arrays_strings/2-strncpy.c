51f3fe3a1fc5b603afe023dd9cb63e4fda8e80fc
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-24 00:04:36 +0300

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+ *_strncpy-that concatenates two strings.
+ *
+ *@dest:string
+ *
+ *@src:string
+ *
+ *@n: bytes from src
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c1;
+
+for (c1 = 0 ; c1 < n && src[c1] != '\0'; c1++)
+	dest[c1] = src[c1];
+
+while (c1 < n)
+{
+dest[c1] = '\0';
+c1++;
+}
+return (dest);
+}
