870719aabb34a3e051c99f6ee570ed0ddff3867b
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-24 01:27:43 +0300

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ *reverse_array- that concatenates two strings.
+ *
+ *@a: array
+ *
+ *@n:array lenght
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int t, j;
+
+for (j = 0 ; j < n  / 2; j++)
+{
+	t = a[n - 1 - j];
+	a[n - 1 - j] = a[j];
+	a[j] = t;
+}
+}
