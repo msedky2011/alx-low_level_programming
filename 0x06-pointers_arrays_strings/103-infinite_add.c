943db276208f5e780a1e3ed1744cfc4d71c94624
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-25 01:32:02 +0300

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,51 +2,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+* _strlen - str
+* @s : string
+* Return: 0
+*/
+
 int _strlen(char *s)
 {
-        int i;
+	int i;
 
-        for (i = 0; *s != '\0'; s++)
-        i++;
+	for (i = 0; *s != '\0'; s++)
+		i++;
 
-        return (i);
+	return (i);
 }
 
-char* reverse_string(char* str) {
-    int length = _strlen(str);
-    int i;
-    char temp;
-    for (i = 0; i < length / 2; i++) {
-        temp = str[i];
-        str[i] = str[length - i - 1];
-        str[length - i - 1] = temp;
-    }
-    return str;
+/**
+* reverse_string - str
+* @str : string
+* Return: 0
+*/
+
+
+char *reverse_string(char *str)
+{
+
+	int length = _strlen(str);
+	int i;
+	char temp;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = temp;
+	}
+	return (str);
 }
 
-char* infinite_add(char* n1, char* n2, char* r, int size_r) {
-    int carry = 0;
-    int index = 0;
-    int n1_length = _strlen(n1);
-    int n2_length = _strlen(n2);
-    int digit1=0;
-    int digit2=0;
-    int sum=0;
-    int digit;
-    while (n1_length > 0 || n2_length > 0 || carry > 0) {
-        digit1 = (n1_length > 0) ? (n1[--n1_length] - '0') : 0;
-        digit2 = (n2_length > 0) ? (n2[--n2_length] - '0') : 0;
+/**
+* infinite_add - infinite
+* @n1: first param
+* @n2: second param
+* @r: result
+* @size_r: buffer size
+* Return: Array to the addition
+*
+*/
 
-        sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        digit = sum % 10;
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
 
-        if (index >= size_r - 1)
-            return 0;
+	int carry = 0;
+	int index = 0;
+	int n1_length = _strlen(n1);
+	int n2_length = _strlen(n2);
+	int digit1 = 0;
+	int digit2 = 0;
+	int sum = 0;
+	int digit;
 
-        r[index++] = digit + '0';
-    }
+	while (n1_length > 0 || n2_length > 0 || carry > 0)
+	{
+		digit1 = (n1_length > 0) ? (n1[--n1_length] - '0') : 0;
+		digit2 = (n2_length > 0) ? (n2[--n2_length] - '0') : 0;
 
-    r[index] = '\0';
-    return reverse_string(r);
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		digit = sum % 10;
+
+		if (index >= size_r - 1)
+			return (0);
+
+		r[index++] = digit + '0';
+	}
+
+	r[index] = '\0';
+	return (reverse_string(r));
 }
