2c5d2a663d338e5a09ec99a719b99ece9b78f6a2
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-25 02:39:19 +0300

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+* print_buffer - point
+*
+* @b : is bee
+* @size : is size
+*
+*
+* Return: void
+*/
+
 void print_buffer(char *b, int size)
 {
 
@@ -8,47 +18,39 @@ void print_buffer(char *b, int size)
 
 	if (size <= 0)
 	{
-
-	printf("\n");
-	return;
+		printf("\n");
+		return;
 	}
-
 	for (i = 0; i < size; i += 10)
 	{
+		printf("%08x ", i);
+		for (j = i; j < i + 10; j += 2)
+		{
+			if (j < size)
+				printf("%02x", (unsigned char)b[j]);
+			else
+			printf("  ");
 
-	printf("%08x ", i);
-	for (j = i; j < i + 10; j += 2)
-	{
-
-		if (j < size)
-		printf("%02x", (unsigned char)b[j]);
-		else
-		printf("  ");
-
-		if (j + 1 < size)
-		printf("%02x ", (unsigned char)b[j + 1]);
-		else
-		printf("   ");
+			if (j + 1 < size)
+				printf("%02x ", (unsigned char)b[j + 1]);
+			else
+				printf("   ");
 	}
-
 	printf(" ");
 	for (j = i; j < i + 10; j++)
 	{
-
 		if (j < size)
 		{
-
-		if (isprint(b[j]))
-			printf("%c", b[j]);
-		else
-			printf(".");
+			if (isprint(b[j]))
+				printf("%c", b[j]);
+			else
+				printf(".");
 		} else
 		{
 
 		printf(" ");
 		}
 	}
-
 	printf("\n");
 	}
 }
