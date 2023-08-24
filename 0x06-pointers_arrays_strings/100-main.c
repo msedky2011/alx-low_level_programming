7e3f6e5cfb702e49581deb4f7907cb472deca922
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-25 02:34:35 +0300

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -8,7 +8,9 @@
  */
 int main(void)
 {
-    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+    /*char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";*/
+    char s[] = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y_Z";
+
     char *p;
 
     p = rot13(s);
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,1 +1,54 @@
-#
+#include <stdio.h>
+#include <ctype.h>
+
+void print_buffer(char *b, int size)
+{
+
+	int i, j;
+
+	if (size <= 0)
+	{
+
+	printf("\n");
+	return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+
+	printf("%08x ", i);
+	for (j = i; j < i + 10; j += 2)
+	{
+
+		if (j < size)
+		printf("%02x", (unsigned char)b[j]);
+		else
+		printf("  ");
+
+		if (j + 1 < size)
+		printf("%02x ", (unsigned char)b[j + 1]);
+		else
+		printf("   ");
+	}
+
+	printf(" ");
+	for (j = i; j < i + 10; j++)
+	{
+
+		if (j < size)
+		{
+
+		if (isprint(b[j]))
+			printf("%c", b[j]);
+		else
+			printf(".");
+		} else
+		{
+
+		printf(" ");
+		}
+	}
+
+	printf("\n");
+	}
+}
