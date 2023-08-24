978d624c4d1b45726202d37e4a2f843a81f34016
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-24 14:38:19 +0300

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,24 @@
 #include "main.h"
+
+/**
+ *_atoi - convert string to intger
+ *@s: array string
+ *Return: intiger
+ */
+void print_number(int n)
+{
+	unsigned int  n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = - n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+		_putchar((n1 % 10) + '0');
+
+}
