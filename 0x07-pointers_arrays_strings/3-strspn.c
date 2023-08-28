d5b881e8fb1d0f800bfa53e37e6a240fab6063e9
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-29 00:46:45 +0300

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,34 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
- */
+*_strspn-find length of accept
+*
+*@s:string
+*
+*@accept :string
+*
+*Return:length
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k=0;	
+	int broken;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{	
+		broken = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				broken = 1;
+				break;
+			}
+		}
+
+		if(!broken)
+			return (k);
+	}
+return(k);
+}
