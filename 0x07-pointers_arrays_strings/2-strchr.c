ee485f08d8553efab5b27be76fb93cfa1a099c92
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-28 22:50:44 +0300

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,21 @@
 #include "main.h"
 /**
+ *_strchr -find c char in string s
+ *
+ *@s:string
  *
  *
+ *@c:char
  *
- *
- *
+ *Return:loction of char if find
+ *else return :null
  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	if (s[i] == c)
+		return (i + s);
+	return ('\0');
+}
