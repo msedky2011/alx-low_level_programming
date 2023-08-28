5dc2a5b59b0e426d2ccaf24947a6eb13202c293b
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-29 01:52:06 +0300

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,40 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
- */
+*_strpbrk-find length of accept
+*
+*@s:string
+*
+*@accept :string
+*
+*Return:length
+*/
+char *_strstr(char *s, char *accept)
+{
+	unsigned int i, j;	
+	int broken = 0;
+
+/* condtion to break before length of substring */ 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+                if (s[i] == accept[0])
+		{
+			broken = 0;
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i+j] != accept[j])
+				{
+					broken = 1;
+					break;
+				}
+			}
+
+			if(!broken)
+			{
+				return s+i;	
+			}
+		}
+
+	}
+
+	return (NULL);
+}
