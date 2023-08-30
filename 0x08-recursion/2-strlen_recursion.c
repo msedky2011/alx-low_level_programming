cdda4300f2aedf72fa2e541dfef1b427fea4b09c
marwa sedky <marwa.sedky2011@gmail.com>
2023-08-30 07:57:51 +0300

    0-puts_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	if (*s != '\0')
+	{
+	i + = _strlen_recursion(s + 1) + 1;
+	}
+	return (i);
+}
