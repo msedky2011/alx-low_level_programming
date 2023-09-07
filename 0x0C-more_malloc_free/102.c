94cce27b5d43b4a1f8f825c2c59243d9d0f664be
marwa sedky <marwa.sedky2011@gmail.com>
2023-09-08 02:03:59 +0300

    101-mul

---
+++ 0x0C-more_malloc_free/102.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	/*int j;*/
+	/*int  mul;*/
+	/*int *result;*/
+
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		/*for(j=0;argv[i][j] != '\0';j++)*/
+		if (argv[i][0] != '\0')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	/*{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}*/
+	return (0);
+}
