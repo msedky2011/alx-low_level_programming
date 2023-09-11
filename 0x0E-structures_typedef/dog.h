c3f3d23889958eaa91b2065151d9b03aa49f6d7a
marwa sedky <marwa.sedky2011@gmail.com>
2023-09-12 02:05:35 +0300

    3-main.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,11 @@ float age;
 char *owner;
 };
 
+/**
+ *dog_t -typedef 
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
