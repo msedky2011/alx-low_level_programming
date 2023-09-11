f560be2a8d447032879dfb57a638d4536b348587
marwa sedky <marwa.sedky2011@gmail.com>
2023-09-12 01:48:30 +0300

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *print_dog-struct dog
+ *@d:a dog structure
+ *
+ *return: nothing
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("age: %f\n", (d->age) ? d->age : 0);
+		printf("owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
