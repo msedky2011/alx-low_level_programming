309fe93315d85e8a60ae3211edcee47d084d310d
marwa sedky <marwa.sedky2011@gmail.com>
2023-09-11 20:01:16 +0300

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main -print the name of the file it was complied
+ *
+ *
+ *Return: always 0 if success
+ *
+ */
+
+int main (void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
