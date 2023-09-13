11bdc7196081a821b50c73e0543d0d69c9c550d8
marwa sedky <marwa.sedky2011@gmail.com>
2023-09-13 22:35:47 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,12 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && size)
+	if (array != NULL)
 	{
 		if (size <= 0)
 			return (-1);
 		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
+			if (cmp(array[i]) > 0)
 				return (i);
 	}
 	return (-1);
