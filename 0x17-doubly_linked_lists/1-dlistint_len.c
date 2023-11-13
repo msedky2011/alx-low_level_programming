46f5889ffd88893485b9186895ebce028e8ae359
marwa sedky <marwa.sedky2011@gmail.com>
2023-11-14 00:25:08 +0200

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,18 @@
-ii
+#include "lists.h"
+
+/**
+ * dlistint_len - print no. of elements in a doubly linked list
+ * @h: first node of list
+ * Return: no. of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while(h)
+	{
+		h = h->next;
+		i++;
+	}
+	return(i);
+}
